073e8883e3a2241d6280c98a9ecf60226c779a4f
Claudio Toyoshi Onohara <claudioonohara@gmail.com>
2020-05-20 07:05:08 -0400

    criando laco na estrutura da diretiva estrututal e acessando valor implicito do loop

--- src/app/directives/for.directive.ts
+++ src/app/directives/for.directive.ts
@@ -11,12 +11,12 @@ import {
 })
 export class ForDirective implements OnInit {
   @Input("myForEm") numbers: number[]
-  @Input("myForUsando") texto: string
-
-  constructor() {}
+  constructor(private container: ViewContainerRef, 
+    private template: TemplateRef<any>) { }
 
   ngOnInit(): void {
-    console.log(this.numbers)
-    console.log(this.texto)
+    for(let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number})
+    }
   }
 }
